2aa482c0e6607d1a3aee462c379ba8e3051f37e9
sseina <moeshuhei@gmail.com>
2016-10-21 01:01:18 +0900

    #戦果登録SQL部分を実装

--- controllers/biz/regist.go
+++ controllers/biz/regist.go
@@ -1,6 +1,7 @@
 package biz
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 
@@ -9,26 +10,77 @@ import (
 
 // 登録データ一覧
 const (
-	Fuel  = "fuel"       // 燃料
-	Ammun = "ammunition" // 弾薬
-	Steel = "steel"      // 鋼材
-	Baux  = "bauxite"    // ボーキサイト
-	Buck  = "bucket"     // 高速修復材
-	DMat  = "dMaterial"  // 開発資材
-	Screw = "screw"      // 改修資材
-	Date  = "date"       // 対象日
+	Fuel  = "fuel"                 // 燃料
+	Ammun = "ammunition"           // 弾薬
+	Steel = "steel"                // 鋼材
+	Baux  = "bauxite"              // ボーキサイト
+	Buck  = "bucket"               // 高速修復材
+	DMat  = "dMaterial"            // 開発資材
+	Bann  = "banner"               // 高速建造材
+	Screw = "screw"                // 改修資材
+	WinSo = "winning_sortie"       // 出撃の勝数
+	DefSo = "defeatting_sortie"    // 出撃の敗数
+	Expe  = "expedition"           // 遠征の回数
+	SuEx  = "successs_expedition"  // 遠征の成功数
+	WinEx = "winning_exercises"    // 演習の勝数
+	DefEx = "defeatting_exercises" // 演習の敗数
+	Veter = "veterans"             // 戦果
+	Rank  = "ranking"              // 順位
+	Date  = "date"                 // 対象日
 )
 
 // Regist Functon
 func Regist(data func(string) string) error {
 	log.Println("Regist Function")
 
-	// DBインサート用の項目をリストにセット(入力チェックも含む)
-	insertData, date, err := configureInput(data)
+	// 入力チェック
+	resources, err := checkForm(data, Fuel, Ammun, Steel, Baux)
 	if err != nil {
 		return err
 	}
+	dbRes := db.Resources{
+		Fuel:  resources[Fuel],
+		Ammun: resources[Ammun],
+		Steel: resources[Steel],
+		Baux:  resources[Baux],
+	}
+	log.Println(dbRes)
+
+	materials, err := checkForm(data, Buck, DMat, Bann, Screw)
+	if err != nil {
+		return err
+	}
+	dbMat := db.Materials{
+		Buck:  materials[Buck],
+		Dmat:  materials[DMat],
+		Screw: materials[Screw],
+		Bann:  materials[Bann],
+	}
+	log.Println(dbMat)
+
+	record, err := checkForm(data, WinSo, DefSo, Expe, SuEx, WinEx, DefEx, Veter, Rank)
+	if err != nil {
+		return err
+	}
+	dbRec := db.Record{
+		WinSo: record[WinSo],
+		DefSo: record[DefSo],
+		Expe:  record[Expe],
+		SuEx:  record[SuEx],
+		WinEx: record[WinEx],
+		DefEx: record[DefEx],
+		Veter: record[Veter],
+		Rank:  record[Rank],
+	}
+	log.Println(dbRec)
+	insertData := db.MaterialList{
+		Resources: dbRes,
+		Materials: dbMat,
+		Record:    dbRec,
+	}
 	log.Println(insertData)
+
+	date := data(Date)
 
 	// DBインサート
 	if err = db.Insert(insertData, date); err != nil {
@@ -40,48 +92,25 @@ func Regist(data func(string) string) error {
 	return nil
 }
 
-// configureInput : POSTメソッドの入力値をリストへ格納する。
-func configureInput(data func(string) string) (db.Material, string, error) {
-	log.Println("configureInput Function")
-	// 入力チェック
-	// TODO: あまりにも冗長なので直す必要あり
-	fuel, err := strconv.Atoi(data(Fuel))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	ammu, err := strconv.Atoi(data(Ammun))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	steel, _ := strconv.Atoi(data(Steel))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	baux, _ := strconv.Atoi(data(Baux))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	buck, _ := strconv.Atoi(data(Buck))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	dMat, _ := strconv.Atoi(data(DMat))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	screw, _ := strconv.Atoi(data(Screw))
-	if err != nil {
-		return db.Material{}, "", err
-	}
-	date := data(Date)
+// checkForm:入力項目を走査し、全て存在すれば結果を返す。1つでも不足していればerrを返す。
+func checkForm(formValue func(string) string, checks ...string) (map[string]int, error) {
 
-	return db.Material{
-		Fuel:  fuel,
-		Ammun: ammu,
-		Steel: steel,
-		Baux:  baux,
-		Buck:  buck,
-		Dmat:  dMat,
-		Screw: screw,
-	}, date, nil
+	result := map[string]int{}
+	for _, check := range checks {
+		// 入力内容を格納し、errの返り値で判定する
+		value := formValue(check)
+		if value == "" {
+			err := fmt.Errorf("Error: %s", "入力チェックエラー")
+			log.Println(err)
+			return map[string]int{}, err
+		}
+		numValue, err := strconv.Atoi(value)
+		if err != nil {
+			log.Println(err)
+			return map[string]int{}, err
+		}
+		result[check] = numValue
+	}
+	log.Println("入力チェック成功: ", result)
+	return result, nil
 }
--- controllers/db/insert.go
+++ controllers/db/insert.go
@@ -7,19 +7,43 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Material : 管理する資材項目
-type Material struct {
+// MaterialList : 管理する資材項目
+type MaterialList struct {
+	Resources Resources
+	Materials Materials
+	Record    Record
+}
+
+// Resources : 資源
+type Resources struct {
 	Fuel  int // 燃料
 	Ammun int // 弾薬
 	Steel int // 鋼材
 	Baux  int // ボーキサイト
+}
+
+// Materials : 資材
+type Materials struct {
 	Buck  int // 高速修復材
 	Dmat  int // 開発資材
 	Screw int // 改修資材
+	Bann  int // 高速建造材
+}
+
+// Record : 戦績
+type Record struct {
+	WinSo int // 出撃の勝数
+	DefSo int // 出撃の敗数
+	Expe  int // 遠征の回数
+	SuEx  int // 遠征の成功数
+	WinEx int // 演習の勝数
+	DefEx int // 演習の敗数
+	Veter int // 戦果
+	Rank  int // 順位
 }
 
 // Insert : 新規データ登録
-func Insert(data Material, date string) error {
+func Insert(data MaterialList, date string) error {
 	log.Println("Insert Function")
 
 	db, err := sql.Open("mysql", "kankore:kongo@/material")
@@ -36,12 +60,23 @@ func Insert(data Material, date string) error {
 
 	// 資源の登録
 	insertQuery := `
-	INSERT INTO
-		resources(fuel, ammunition, steel, bauxite, target_date)
-	VALUES
-		(?, ?, ?, ?, ?)
-	`
-	if _, err = tx.Exec(insertQuery, data.Fuel, data.Ammun, data.Steel, data.Baux, date); err != nil {
+		INSERT INTO
+			resources(
+				fuel, 
+				ammunition, 
+				steel, 
+				bauxite, 
+				target_date)
+		VALUES
+			(?, ?, ?, ?, ?)
+		`
+	if _, err = tx.Exec(
+		insertQuery,
+		data.Resources.Fuel,
+		data.Resources.Ammun,
+		data.Resources.Steel,
+		data.Resources.Baux,
+		date); err != nil {
 		log.Println("資源登録エラー")
 		tx.Rollback()
 		return err
@@ -49,13 +84,55 @@ func Insert(data Material, date string) error {
 
 	//　開発資材の登録
 	insertQuery = `
-	INSERT INTO
-		materials(bucket, dMaterial, screw, target_date)
-	VALUES
-		(?, ?, ?, ?)
-	`
-	if _, err = tx.Exec(insertQuery, data.Buck, data.Dmat, data.Screw, date); err != nil {
+		INSERT INTO
+			materials(
+				bucket, 
+				dMaterial, 
+				screw, 
+				banner, 
+				target_date)
+		VALUES
+			(?, ?, ?, ?, ?)
+		`
+	if _, err = tx.Exec(
+		insertQuery,
+		data.Materials.Buck,
+		data.Materials.Dmat,
+		data.Materials.Screw,
+		data.Materials.Bann,
+		date); err != nil {
 		log.Println("開発資材登録エラー")
+		tx.Rollback()
+		return err
+	}
+
+	insertQuery = `
+		Insert INTO
+			record(
+				winning_sortie, 
+				defeatting_sortie, 
+				expedition, 
+				successs_expedition,
+				winning_exercises,
+				defeatting_exercises,
+				veterans,
+				ranking,
+				target_date)
+		VALUES
+			(?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	if _, err = tx.Exec(
+		insertQuery,
+		data.Record.WinSo,
+		data.Record.DefSo,
+		data.Record.Expe,
+		data.Record.SuEx,
+		data.Record.WinEx,
+		data.Record.DefEx,
+		data.Record.Veter,
+		data.Record.Rank,
+		date); err != nil {
+		log.Println("戦績登録エラー")
 		tx.Rollback()
 		return err
 	}
